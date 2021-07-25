fa66d398adc4ba2967333c5c0c798dee7cdf5f77
Paramveer Soundane <paramveersoundane@gmail.com>
2021-07-25 17:41:54 +0530

    Assignments added
    Assignment -1 CoronaUniverse, Assignment CoronaEpicenter

---
+++ CoronaEpicenter.java
@@ -0,0 +1,120 @@
+package ds.graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CoronaEpicenter {
+	// insert Road into Adj List
+	static void insertRoad(HashMap<Integer, ArrayList<Integer>> adjList, int cityA, int cityB) {
+		//connect A->B
+		ArrayList<Integer> roads=adjList.getOrDefault(cityA, new ArrayList<Integer>());
+		roads.add(cityB);
+		adjList.put(cityA,roads);
+		// connect B->A
+		roads=adjList.getOrDefault(cityB, new ArrayList<Integer>());
+		roads.add(cityA);
+		adjList.put(cityB,roads);
+	}
+	// Add x nearest node to set
+	static Set<Integer> BFSNormal(HashMap<Integer, ArrayList<Integer>> adjList,int hotspot, int x){
+		
+		Set<Integer> set= new HashSet<Integer>();
+		Queue<Integer> q=new LinkedList<Integer>();
+		Queue<Integer> distance=new LinkedList<Integer>();
+		
+		q.add(hotspot);
+		int y=0;
+		distance.add(y);
+		while(!q.isEmpty()) {
+			int v=q.poll();
+			int d=distance.poll();
+			if(d==x) {
+				continue;
+			}
+			d=d+1;
+			ArrayList<Integer> temp = adjList.get(v);
+			//System.out.println(temp);
+			for(int i: temp) {
+				if(set.contains(i)) {
+					continue;
+				}
+			    set.add(i);
+				q.add(i);
+				distance.add(d);
+			}
+			
+		}
+		return set;
+		
+	}
+	// Add x nearest node to set which also contains in tempset
+	static Set<Integer> BFS(HashMap<Integer, ArrayList<Integer>> adjList,int hotspot, int x,Set<Integer> tempset){
+		
+		Set<Integer> set= new HashSet<Integer>();
+		Queue<Integer> q=new LinkedList<Integer>();
+		int y=0;
+		Queue<Integer> distance=new LinkedList<Integer>();
+		q.add(hotspot);
+		distance.add(y);
+		if(tempset.contains(hotspot)) {
+			set.add(hotspot);
+		}
+		
+		while(!q.isEmpty()) {
+			int v=q.poll();
+			int d=distance.poll();
+			if(d==x) {
+				continue;
+			}
+			d=d+1;
+			ArrayList<Integer> temp = adjList.get(v);
+			for(int i: temp) {
+				if(set.contains(i)) {
+					continue;
+				}
+				
+				if(tempset.contains(i)) {
+					set.add(i);
+				}
+				q.add(i);
+				distance.add(d);
+			}
+			
+		}
+		
+		return set;
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int h=scan.nextInt();
+		int x=scan.nextInt();
+		HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<Integer, ArrayList<Integer>>();
+		ArrayList<Integer> hotspots= new ArrayList<Integer>();
+		for(int i=0;i<h;i++) {
+			int temp=scan.nextInt();
+			hotspots.add(temp);
+		}
+		for(int i=1;i<=n-1;i++) {
+			int cityA=scan.nextInt();
+			int cityB=scan.nextInt();
+			insertRoad(adjList, cityA, cityB);
+		}
+		Set<Integer> set= BFSNormal(adjList, hotspots.get(0), x);
+
+		for(int i=1;i<hotspots.size();i++) {
+			set=BFS(adjList, hotspots.get(i), x, set);
+		}
+		scan.close();
+		System.out.println(set);
+		System.out.println(set.size());
+	}
+
+}
---
+++ CoronaUniverse.java
@@ -0,0 +1,72 @@
+package ds.graphs;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Portal implements Comparator<Portal>{
+    int universe;
+    int time;
+    Portal(){}
+    Portal(int v,int w){
+        this.universe=v;
+        this.time=w;
+    }
+    @Override
+    public int compare(Portal n1, Portal n2){
+        return n1.time - n2. time;
+    }
+    public String toString(){
+        return "Universe: "+this.universe+", Time: " + this.time;
+    }
+}
+
+public class CoronaUniverse {
+	
+	public static void main(String[] args) {
+		Scanner scan=new  Scanner(System.in);
+		int n=scan.nextInt(); // number of universe
+		int m=scan.nextInt(); // number of portals
+		HashMap<Integer,ArrayList<Portal>> adjList=new HashMap<Integer, ArrayList<Portal>>();
+		for(int i=1;i<=m;i++) {
+			int fromUniverse=scan.nextInt();
+			int toUniverse=scan.nextInt();
+			int time=scan.nextInt();
+			ArrayList<Portal> portals=adjList.getOrDefault(fromUniverse, new  ArrayList<Portal>());
+			portals.add(new Portal(toUniverse, time));
+			adjList.put(fromUniverse, portals);
+		}
+		int hm[] = new int[n+1];
+		for(int i=1;i<=n;i++) {
+        	hm[i]=-1;
+        }
+		int result=shortestPath(adjList, n, hm);
+		scan.close();
+		System.out.println(result);
+	}
+	// Djikstra Algorithm
+	static int shortestPath(HashMap<Integer,ArrayList<Portal>> adjList, int n, int hm[]) {
+		
+		PriorityQueue<Portal> pq=new PriorityQueue<Portal>(new Portal());
+		hm[1]=0;
+		pq.add(new Portal(1,0));
+		while(!pq.isEmpty()) {
+			Portal p=pq.poll();
+        	ArrayList<Portal> portals = adjList.get(p.universe);
+        	if(portals==null) {
+        		continue;
+        	}
+        	for(Portal u: portals) {
+        		int w=p.time+u.time;
+        		if(hm[u.universe]>w || hm[u.universe]==-1) {
+        			pq.add(new Portal(u.universe,w));
+        			hm[u.universe]=w;
+        		}
+        	}
+		}
+		return hm[n];
+		
+	}
+}
